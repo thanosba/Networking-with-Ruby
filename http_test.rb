5a8ca356d110fb67fd0892129fc4adde26db10d4
Athanasios Balanikas <athanasios.balanikas@yahoo.gr>
2017-08-19 15:09:57 +0300

    Add files via upload

---
+++ http_test.rb
@@ -0,0 +1,24 @@
+# Script 15.4 - http_test.rb
+
+# Include the library:
+require 'open-uri'
+
+# Start a block:
+begin
+
+  # Open the resource, providing basic authentication:
+  open('http://www.dmcinsights.com/ruby/ruby_test.php', :http_basic_authentication => ['Chicago', 'Bears']) { |http|
+  
+    # Display some information about the page:
+    puts "Content-type: #{http.content_type}"
+    puts "Status: #{http.status[1]}"
+    puts '--------------'
+  
+    # Display the page's content:
+    puts http.read
+
+  }
+
+rescue => e
+  puts "The connection could not be made: #{e.message}"
+end 
